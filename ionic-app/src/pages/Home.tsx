ca86667a35ad1e715cc1c42634cc0c6b1a5417d9
Mazahir Haroon <mazahirharoon@gmail.com>
2025-12-12 15:52:56 +0530

    feat(mobile-app): update Home screen title to new app name ScrapGo

--- ionic-app/src/pages/Home.tsx
+++ ionic-app/src/pages/Home.tsx
@@ -1,4 +1,10 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
@@ -7,13 +13,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>ScrapGo</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        <IonHeader collapse='condense'>
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size='large'>Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
